2f3bedf2c43edcc8861861875d7aa2037315a774
Novoz <nikita_novoshen@mail.ru>
2021-02-17 23:53:39 +0300

    web2

---
+++ frontend/src/views/content.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-9 p-lg-5 mx-auto my-5">
+      <img class="mb-4" src="img/BvsT.jfif" alt="" width="300" height="300">
+      <h1 class="display-4 font-weight-normal">Joe Biden VS Donald Trump </h1>
+      <p class="lead font-weight-normal"zzzz>President Donald Trump, 74, and Democratic challenger Joe Biden, 77, each have more than seven decades of personal and professional experience behind them</p>
+      <a class="btn btn-outline-secondary" href="#">Continue reading...</a>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "content"
+}
+</script>
+
+<style scoped>
+
+</style>
